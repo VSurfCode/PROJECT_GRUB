95f1ade8eef8b01b7174deb22b2009f8bf1e998f
AMarstonCode <amarston@perseverenow.org>
2025-03-15 10:49:00 -0500

    init commit

---
+++ grubGoblin/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import GoblinLogo from './assets/GrubGoblinLogo2.png'
+
+function App() {
+
+  return (
+    <>
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <img src={GoblinLogo} alt="" width={200} />
+      <h1>Grub Goblin</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
